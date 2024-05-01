40030c38229bc1b5b726649358812fd93971f766
David Conner <robotics@cnu.edu>
2024-05-01 17:06:07 -0400

    Rebase from 'rpm/humble/flexbe_core'

--- flexbe_core/logger.py
+++ flexbe_core/logger.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2023 Philipp Schillinger, Team ViGIR, Christopher Newport University
+# Copyright 2024 Philipp Schillinger, Team ViGIR, Christopher Newport University
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -31,7 +31,9 @@
 
 """Realize behavior-specific logging."""
 
+from rclpy.exceptions import ParameterNotDeclaredException
 from rclpy.node import Node
+from rclpy.duration import Duration
 
 from flexbe_msgs.msg import BehaviorLog
 
@@ -47,6 +49,10 @@ class Logger:
 
     LOGGING_TOPIC = 'flexbe/log'
 
+    # max number of items in last logged dict (used for log_throttle)
+    MAX_LAST_LOGGED_SIZE = 1024
+    LAST_LOGGED_CLEARING_RATIO = 0.2
+
     _pub = None
     _node = None
 
@@ -54,6 +60,26 @@ class Logger:
     def initialize(node: Node):
         Logger._node = node
         Logger._pub = node.create_publisher(BehaviorLog, Logger.LOGGING_TOPIC, 100)
+        Logger._last_logged = {}
+
+        # Optional parameters that can be defined
+        try:
+            size_param = node.get_parameter("max_throttle_logging_size")
+            if size_param.type_ == size_param.Type.INTEGER:
+                Logger.MAX_LAST_LOGGED_SIZE = size_param.value
+        except ParameterNotDeclaredException as exc:
+            pass
+
+        try:
+            clear_param = node.get_parameter("throttle_logging_clear_ratio")
+            if clear_param.type_ in (clear_param.Type.INTEGER, clear_param.Type.DOUBLE):
+                Logger.LAST_LOGGED_CLEARING_RATIO = clear_param.value
+        except ParameterNotDeclaredException as exc:
+            pass
+
+        Logger._node.get_logger().debug(f"Enable throttle logging option with "
+                                        f"max size={Logger.MAX_LAST_LOGGED_SIZE} "
+                                        f"clear ratio={Logger.LAST_LOGGED_CLEARING_RATIO}")
 
     @staticmethod
     def log(text: str, severity: int):
@@ -66,6 +92,25 @@ class Logger:
         Logger._pub.publish(msg)
         # also log locally
         Logger.local(text, severity)
+
+    @staticmethod
+    def log_throttle(period: float, text: str, severity: int):
+        # create unique identifier for each logging message
+        log_id = f"{severity}_{text}"
+        time_now = Logger._node.get_clock().now()
+        # only log when it's the first time or period time has passed for the logging message
+        if log_id not in Logger._last_logged.keys() or \
+           time_now - Logger._last_logged[log_id] > Duration(seconds=period):
+            Logger.log(text, severity)
+            Logger._last_logged.update({log_id: time_now})
+
+        if len(Logger._last_logged) > Logger.MAX_LAST_LOGGED_SIZE:
+            # iterate through last logged items, sorted by the timestamp (oldest last)
+            clear_size = Logger.MAX_LAST_LOGGED_SIZE * (1 - Logger.LAST_LOGGED_CLEARING_RATIO)
+            for i, log in enumerate(sorted(Logger._last_logged.items(), key=lambda item: item[1], reverse=True)):
+                # remove defined percentage of oldest items
+                if i > clear_size:
+                    Logger._last_logged.pop(log[0])
 
     @staticmethod
     def local(text: str, severity: int):
@@ -87,57 +132,81 @@ class Logger:
     # NOTE: Below text strings can only have single % symbols if they are being treated
     # as format strings with appropriate arguments (otherwise replace with %% for simple string without args)
     @staticmethod
-    def logdebug(text, *args):
+    def logdebug(text: str, *args):
         Logger.log(text % args, Logger.REPORT_DEBUG)
 
     @staticmethod
-    def loginfo(text, *args):
+    def loginfo(text: str, *args):
         Logger.log(text % args, Logger.REPORT_INFO)
 
     @staticmethod
-    def logwarn(text, *args):
+    def logwarn(text: str, *args):
         Logger.log(text % args, Logger.REPORT_WARN)
 
     @staticmethod
-    def loghint(text, *args):
+    def loghint(text: str, *args):
         Logger.log(text % args, Logger.REPORT_HINT)
 
     @staticmethod
-    def logerr(text, *args):
+    def logerr(text: str, *args):
         Logger.log(text % args, Logger.REPORT_ERROR)
 
     @staticmethod
-    def localdebug(text, *args):
+    def logdebug_throttle(period: float, text: str, *args):
+        Logger.log_throttle(period, text % args, Logger.REPORT_DEBUG)
+
+    @staticmethod
+    def loginfo_throttle(period: float, text: str, *args):
+        Logger.log_throttle(period, text % args, Logger.REPORT_INFO)
+
+    @staticmethod
+    def logwarn_throttle(period: float, text: str, *args):
+        Logger.log_throttle(period, text % args, Logger.REPORT_WARN)
+
+    @staticmethod
+    def loghint_throttle(period: float, text: str, *args):
+        Logger.log_throttle(period, text % args, Logger.REPORT_HINT)
+
+    @staticmethod
+    def logerr_throttle(period: float, text: str, *args):
+        Logger.log_throttle(period, text % args, Logger.REPORT_ERROR)
+
+    @staticmethod
+    def localdebug(text: str, *args):
         Logger.local(text % args, Logger.REPORT_DEBUG)
 
     @staticmethod
-    def localinfo(text, *args):
+    def localinfo(text: str, *args):
         Logger.local(text % args, Logger.REPORT_INFO)
 
     @staticmethod
-    def localwarn(text, *args):
+    def localwarn(text: str, *args):
         Logger.local(text % args, Logger.REPORT_WARN)
 
     @staticmethod
-    def localerr(text, *args):
+    def localhint(text: str, *args):
+        Logger.local(text % args, Logger.REPORT_HINT)
+
+    @staticmethod
+    def localerr(text: str, *args):
         Logger.local(text % args, Logger.REPORT_ERROR)
 
     @staticmethod
-    def debug(text, *args):
+    def debug(text: str, *args):
         Logger.logdebug(text, *args)
 
     @staticmethod
-    def info(text, *args):
+    def info(text: str, *args):
         Logger.loginfo(text, *args)
 
     @staticmethod
-    def warning(text, *args):
+    def warning(text: str, *args):
         Logger.logwarn(text, *args)
 
     @staticmethod
-    def hint(text, *args):
+    def hint(text: str, *args):
         Logger.loghint(text, *args)
 
     @staticmethod
-    def error(text, *args):
+    def error(text: str, *args):
         Logger.logerr(text, *args)
