8f463da2e2b290dc44e8d0a8932af08cf1cf3ff3
David Conner <robotics@cnu.edu>
2024-06-06 12:34:48 -0400

    Rebase from 'rpm/iron/flexbe_onboard'

--- launch/behavior_onboard.launch.py
+++ launch/behavior_onboard.launch.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Philipp Schillinger, Team ViGIR, Christopher Newport University
+# Copyright 2024 Philipp Schillinger, Team ViGIR, Christopher Newport University
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -26,29 +26,31 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+"""Launch file for FlexBE Onboard Behavior Executive."""
 
 from launch import LaunchDescription
-from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 
+from launch_ros.actions import Node
+
 
 def generate_launch_description():
-
+    """Generate launch description."""
     return LaunchDescription([
-        DeclareLaunchArgument("log_enabled", default_value="False"),
-        DeclareLaunchArgument("log_folder", default_value="~/.flexbe_logs"),
-        DeclareLaunchArgument("log_serialize", default_value="yaml"),
-        DeclareLaunchArgument("log_level", default_value="INFO"),
-        DeclareLaunchArgument("use_sim_time", default_value="False"),
-        DeclareLaunchArgument("enable_clear_imports", default_value="False",
-                              description="Delete behavior-specific module imports after execution."),
+        DeclareLaunchArgument('log_enabled', default_value='False'),
+        DeclareLaunchArgument('log_folder', default_value='~/.flexbe_logs'),
+        DeclareLaunchArgument('log_serialize', default_value='yaml'),
+        DeclareLaunchArgument('log_level', default_value='INFO'),
+        DeclareLaunchArgument('use_sim_time', default_value='False'),
+        DeclareLaunchArgument('enable_clear_imports', default_value='False',
+                              description='Delete behavior-specific module imports after execution.'),
         Node(
-            name="behavior", package="flexbe_onboard", executable="start_behavior", output="screen",
-            parameters=[{"log_enabled": LaunchConfiguration("log_enabled"),
-                         "log_folder": LaunchConfiguration("log_folder"),
-                         "log_serialize": LaunchConfiguration("log_serialize"),
-                         "log_level": LaunchConfiguration("log_level"),
-                         "enable_clear_imports": LaunchConfiguration("enable_clear_imports"),
-                         "use_sim_time": LaunchConfiguration("use_sim_time")}])
+            name='behavior', package='flexbe_onboard', executable='start_behavior', output='screen',
+            parameters=[{'log_enabled': LaunchConfiguration('log_enabled'),
+                         'log_folder': LaunchConfiguration('log_folder'),
+                         'log_serialize': LaunchConfiguration('log_serialize'),
+                         'log_level': LaunchConfiguration('log_level'),
+                         'enable_clear_imports': LaunchConfiguration('enable_clear_imports'),
+                         'use_sim_time': LaunchConfiguration('use_sim_time')}])
     ])
