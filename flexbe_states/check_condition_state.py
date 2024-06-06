c99c48f04029d35cc2d96779b89547fcc5ad5e81
David Conner <robotics@cnu.edu>
2024-06-06 14:31:22 -0400

    Rebase from 'release/rolling/flexbe_states'

--- flexbe_states/check_condition_state.py
+++ flexbe_states/check_condition_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2023 Philipp Schillinger, Team ViGIR, Christopher Newport University
+# Copyright 2024 Philipp Schillinger, Team ViGIR, Christopher Newport University
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
@@ -28,7 +28,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-
+"""CheckConditionState."""
 from flexbe_core import EventState, Logger
 
 
@@ -48,27 +48,30 @@ class CheckConditionState(EventState):
     """
 
     def __init__(self, predicate):
+        """Initialize state."""
         super(CheckConditionState, self).__init__(outcomes=['true', 'false'],
                                                   input_keys=['input_value'])
         self._predicate = None
         if callable(predicate):
             self._predicate = predicate
         elif isinstance(predicate, str):
-            if "__" in predicate:
+            if '__' in predicate:
                 Logger.logwarn(f"potentially unsafe code in predicate '{predicate}' - use caution if executing!")
             try:
                 self._predicate = eval(predicate)  # Assumes behavior is from a trusted source!
             except Exception as exc:  # pylint: disable=W0703
-                Logger.logwarn(f"Failed to convert predicate to callable function!\n  {str(exc)}")
+                Logger.logwarn(f'Failed to convert predicate to callable function!\n  {str(exc)}')
 
         self._outcome = 'false'
 
     def execute(self, userdata):
+        """Execute the condition outcome."""
         return self._outcome
 
     def on_enter(self, userdata):
+        """Evaluate condition on entering state."""
         try:
             self._outcome = 'true' if self._predicate(userdata.input_value) else 'false'
         except Exception as exc:  # pylint: disable=W0703
-            Logger.logwarn(f"Failed to execute condition function!\n  {str(exc)}")
+            Logger.logwarn(f'Failed to execute condition function!\n  {str(exc)}')
             self._outcome = 'false'
