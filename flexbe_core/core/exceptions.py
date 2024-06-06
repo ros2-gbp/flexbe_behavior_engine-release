9c89fab04c50e25fb0bd1f8b2f120e00a78cebe2
David Conner <robotics@cnu.edu>
2024-06-06 14:32:36 -0400

    Rebase from 'rpm/rolling/flexbe_core'

--- flexbe_core/core/exceptions.py
+++ flexbe_core/core/exceptions.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# Copyright 2023 Philipp Schillinger, Team ViGIR, Christopher Newport University
+# Copyright 2024 Philipp Schillinger, Team ViGIR, Christopher Newport University
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
