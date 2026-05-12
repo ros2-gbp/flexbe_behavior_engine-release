db8e332af9970f88e8ea7c9c32051e7c48d0b6fa
David Conner <robotics@cnu.edu>
2026-05-11 20:22:09 -0400

    Rebase from 'upstream'

--- flexbe_states/flexbe_states/log_key_state.py
+++ flexbe_states/flexbe_states/log_key_state.py
@@ -45,7 +45,7 @@ class LogKeyState(EventState):
     -- text         string    Format string of message to be logged to the terminal; e.g., 'Counter value:  {}'.
     -- severity     uint8     Type of logging (Logger.REPORT_INFO / WARN / HINT / ERROR)
 
-    #> data         object    The data provided to be printed in the message. The exact type depends on the request.
+    ># data         object    The data provided to be printed in the message. The exact type depends on the request.
 
     <= done                   Indicates that the message has been logged.
     """
@@ -64,4 +64,7 @@ class LogKeyState(EventState):
 
     def on_enter(self, userdata):
         """Log upon entering the state."""
-        Logger.log(self._text.format(userdata.data), self._severity)
+        try:
+            Logger.log(self._text.format(userdata.data), self._severity)
+        except Exception as exc:  # pylint: disable=W0703
+            Logger.logwarn(f"LogKeyState failed to format message '{self._text}': {exc}")
